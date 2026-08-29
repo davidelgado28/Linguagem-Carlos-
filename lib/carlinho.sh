795f70bde3340d72ab83cb516cceb8e95682f361
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-29 19:12:51 -0300

    Update carlinho.sh

--- lib/carlinho.sh
+++ lib/carlinho.sh
@@ -1,1 +1,13 @@
+#ifndef CARLINHO_H
+#define CARLINHO_H
 
+// Biblioteca padrão da linguagem Carlos
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#endif
